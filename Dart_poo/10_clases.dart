072f4b36027ab99bd16d27a5c11be859b49d880c
Erick <202100366a@utea.edu.pe>
2024-06-12 21:46:00 -0500

    Video10

---
+++ Dart_poo/10_clases.dart
@@ -0,0 +1,18 @@
+void main(List<String> args) {
+  print(Colores.values);
+
+  Colores.values.forEach((item){
+    //print(item.index);
+
+    print(item.name);
+  });
+
+}
+
+enum Colores{
+
+  rojo,
+  azul,
+  amarillo
+
+}
