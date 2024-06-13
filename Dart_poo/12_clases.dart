cb97edc023901d0cdc945932908824e254789c17
Erick <202100366a@utea.edu.pe>
2024-06-12 21:58:50 -0500

    Video12

---
+++ Dart_poo/12_clases.dart
@@ -0,0 +1,21 @@
+
+import 'dart:math';
+void main(List<String> args) {
+
+    print(e);
+    print(pi);
+    print(sqrt(4));
+    print(sqrt2);
+
+    print(sin(1));
+    print(cos(2));
+    print(tan(1));
+
+    print(pow(2,5));
+
+    print(max(4,6));
+
+    print(min(4,6));
+
+    print(Random().nextInt(10));
+}
