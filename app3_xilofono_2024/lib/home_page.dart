23ede354d855e292dd6aa59d190ef593b570ddf6
Erick <202100366a@utea.edu.pe>
2024-06-25 07:11:49 -0500

    dependencias

--- app3_xilofono_2024/lib/home_page.dart
+++ app3_xilofono_2024/lib/home_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class HomaPage extends StatelessWidget {
-  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold();
