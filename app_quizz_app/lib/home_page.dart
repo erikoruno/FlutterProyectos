c882b1cad149e42341c9e51d08f0828bb0e87899
Erick <202100366a@utea.edu.pe>
2024-06-27 07:17:12 -0500

    appvalido

--- app_quizz_app/lib/home_page.dart
+++ app_quizz_app/lib/home_page.dart
@@ -6,6 +6,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
+        backgroundColor: Colors.black,
         title: const Text(
           "QuizzApp",
           style: TextStyle(
@@ -13,6 +14,61 @@ class HomePage extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
+        centerTitle: true,
+        elevation: 30.0,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Center(
+            child: Text(
+              "El hombre llego a la luna?",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 500.0,
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.greenAccent,
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Verdadero",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.pink,
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Falso",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
