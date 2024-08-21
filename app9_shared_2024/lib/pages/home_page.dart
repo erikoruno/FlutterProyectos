ac22a5f317c7dd2f1ff75c267f50f32ef11a22b5
Erick <202100366a@utea.edu.pe>
2024-08-21 06:37:33 -0500

    shared_casi_fin

--- app9_shared_2024/lib/pages/home_page.dart
+++ app9_shared_2024/lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:app9_shared_2024/widgets/my_drawer_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -17,84 +18,7 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
-      drawer: Drawer(
-        child: Column(
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: NetworkImage(
-                      "https://images.pexels.com/photos/73873/star-clusters-rosette-nebula-star-galaxies-73873.jpeg?auto=compress&cs=tinysrgb&w=600"),
-                ),
-              ),
-              child: SizedBox(
-                width: double.infinity,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    CircleAvatar(
-                      radius: 30,
-                      backgroundColor: Colors.white,
-                      backgroundImage: NetworkImage(
-                          "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"),
-                    ),
-                    Text(
-                      "Fiorella Gonzales Torres",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Text(
-                      "Administrador",
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.normal,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.person,
-                color: Colors.black54,
-              ),
-              title: Text("My Profile"),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.file_copy,
-                color: Colors.black54,
-              ),
-              title: Text("Portafile"),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.lock,
-                color: Colors.black54,
-              ),
-              title: Text("Change Password"),
-            ),
-            Divider(
-              indent: 12,
-              endIndent: 12,
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.exit_to_app,
-                color: Colors.black54,
-              ),
-              title: Text("Logout"),
-            ),
-          ],
-        ),
-      ),
+      drawer: MyDrawerWidget(),
       body: Padding(
         padding: const EdgeInsets.only(left: 16.0),
         child: Column(
@@ -108,18 +32,22 @@ class HomePage extends StatelessWidget {
               ),
             ),
             SizedBox(
-              height: 50,
+              height: 30,
             ),
-            Divider(
-              indent: 12,
-              endIndent: 12,
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Ingrese texto',
+                // Esto añade un borde alrededor del campo de texto
+              ),
             ),
             SizedBox(
-              height: 50,
+              height: 30,
             ),
-            Divider(
-              indent: 12,
-              endIndent: 12,
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Ingrese texto',
+                // Esto añade un borde alrededor del campo de texto
+              ),
             ),
             SizedBox(
               height: 50,
---
+++ app9_shared_2024/lib/widgets/my_drawer_widget.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class MyDrawerWidget extends StatelessWidget {
+  const MyDrawerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(
+                    "https://images.pexels.com/photos/73873/star-clusters-rosette-nebula-star-galaxies-73873.jpeg?auto=compress&cs=tinysrgb&w=600"),
+              ),
+            ),
+            child: SizedBox(
+              width: double.infinity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Colors.white,
+                    backgroundImage: NetworkImage(
+                        "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"),
+                  ),
+                  Text(
+                    "Fiorella Gonzales Torres",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    "Administrador",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.person,
+              color: Colors.black54,
+            ),
+            title: Text("My Profile"),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.file_copy,
+              color: Colors.black54,
+            ),
+            title: Text("Portafile"),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.lock,
+              color: Colors.black54,
+            ),
+            title: Text("Change Password"),
+          ),
+          Divider(
+            indent: 12,
+            endIndent: 12,
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.exit_to_app,
+              color: Colors.black54,
+            ),
+            title: Text("Logout"),
+          ),
+        ],
+      ),
+    );
+  }
+}
